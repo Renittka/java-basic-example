7a17253e30604d33f07b5f76aba49fd09a3fdf5e
Anna Ogorodova <aogorodova@dar.io>
2023-10-07 09:46:53 +0200

    Added new classes

---
+++ src/HomeWork1.java
@@ -0,0 +1,26 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        // System.out.println("I am learning Java");
+        // System.out.println(100);
+        // System.out.println(100 + 200);
+        System.out.println("5 + 6 = " + (5 + 6));
+
+
+        // Task 1
+        System.out.print(100); // Print 100
+        System.out.print(100 + 200);
+        System.out.print("5 + 6 = " + (5 + 6));
+
+        // Task 2
+        // Это однострочный комментарий
+        System.out.println();
+        /*
+        Это многострочный комментарий
+        System.out.println();
+        System.out.println();
+        System.out.println();
+         */
+
+    }
+}
